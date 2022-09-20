16bb29897b7c3bf9d45f147f79e1084a77d456e5
Toriq Setiawan <toriqbagus@gmail.com>
2022-09-20 07:57:07 +0700

    [Toriq] integration detail movie

---
+++ app/Http/Controllers/User/MovieController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class MovieController extends Controller
+{
+    public function show(Movie $movie)
+    {
+        return Inertia::render(
+            'User/Dashboard/Movie/Show',
+            compact('movie')
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\MovieController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -30,6 +31,7 @@ Route::redirect('/', '/login');
 
 Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function() {
     Route::get('/', [DashboardController::class, 'index'])->name('index');
+    Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
 });
 
 Route::prefix('prototype')->name('prototype.')->group(function() {
